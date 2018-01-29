329ae2cf5a88b74c9708b28cfda520d5bb681961
Lasse Strauss <lasse@strauss-foto.dk>
2018-01-29 08:35:37 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
+import { TestingPageComponent } from './testing-page/testing-page.component';
 
 // AoT requires an exported function for factories
 export function createTranslateLoader(http: HttpClient) {
@@ -32,7 +33,7 @@ export function createTranslateLoader(http: HttpClient) {
         }),
         AppRoutingModule
     ],
-    declarations: [AppComponent],
+    declarations: [AppComponent, TestingPageComponent],
     providers: [AuthGuard],
     bootstrap: [AppComponent]
 })
